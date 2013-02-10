3d7e6d1f41819e81c7a5d57f4674ef518b2e936a
Tobias Koppers <tobias.koppers@googlemail.com>
2013-02-10 21:25:15 +0100

    use global

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ module.exports = function() {};
 module.exports.pitch = function(remainingRequest) {
 	if(!this.query) throw new Error("query parameter is missing");
 	return "module.exports = " +
-		"this[" + JSON.stringify(this.query.substr(1)) + "] = " +
+		"global[" + JSON.stringify(this.query.substr(1)) + "] = " +
 		"require(" + JSON.stringify(remainingRequest) + ");";
 };
