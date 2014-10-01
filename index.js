5e3fb254962ff57ced9de416e445ad93670923c0
Cuong Vo <cvo@viewpost.com>
2014-10-01 12:24:47 -0400

    Splits on '.' to expose nested properties

--- index.js
+++ index.js
@@ -2,11 +2,24 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+
+function accesorString(value) {
+	var depths = value.split(".");
+	var length = depths.length;
+	var result = "";
+	
+	for (var i = 0; i < length; i++) {
+		result += "[" + JSON.stringify(depths[i]) + "]";
+	}
+
+	return result;
+}
+
 module.exports = function() {};
 module.exports.pitch = function(remainingRequest) {
 	this.cacheable && this.cacheable();
 	if(!this.query) throw new Error("query parameter is missing");
 	return "module.exports = " +
-		"global[" + JSON.stringify(this.query.substr(1)) + "] = " +
+		"global" + accesorString(this.query.substr(1)) + " = " +
 		"require(" + JSON.stringify("-!" + remainingRequest) + ");";
 };
