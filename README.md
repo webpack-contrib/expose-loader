5600f0e7be131b316d1b7384305e5efb67b24a2f
Sasha Kovalyov <alexander.kovaleff@gmail.com>
2015-02-23 13:59:16 +0100

    introduce `module`, `ns` query params
    fix indentation

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ require("expose?libraryName!./file.js");
 
 This line works to expose React to the web browser to enable the Chrome React devtools:
 
-```
+```js
 require("expose?React!react");
 ```
 
@@ -18,7 +18,7 @@ Thus, `window.React` is then available to the Chrome React devtools extension.
 
 Alternately, you can set this in your config file:
 
-```
+```js
 module: {
   loaders: [
     { test: require.resolve("react"), loader: "expose?React" }
@@ -31,6 +31,44 @@ processing -- check the node.js docs instead). `require.resolve` gives you the
 absolute path to the module ("/.../app/node_modules/react/react.js"). So the
 expose only applies to the react module. And it's only exposed when used in the
 bundle.
+
+You can also configure a module name based on its path, filename and extension.
+See [`loaderUtils#interpolateName`](https://github.com/webpack/loader-utils#interpolatename) function for the full list of available tokens.
+The module name can be specified using `module` query param, like this:
+
+```js
+module: {
+  loaders: [
+    { test: /app\/.*\.js$/, loader: "'expose?module=[path][name]'" }
+  ]
+}
+```
+
+In case `module` query param is provided, two additional ones may be added:
+ - `ns` - defines a global namespace all exposed modules are added to.
+ - `upperCamelCase` - enable this flag when full file path should be converted to upper camel case.
+
+The following example demonstrates the usage of those two params.
+
+```js
+module: {
+  loaders: [
+    {
+      test: /app\/.*\.js$/,
+      loader: 'expose?ns=App&module=[path][name]&upperCamelCase'
+    }
+  ]
+}
+```
+
+Provided that your app has the following directories structure,
+```js
+|-app
+  |-models
+    |-user
+    |-user-stream-item
+```
+you can get access to the modules listed above just by typing `App.Models.User` and `App.Models.UserStreamItem` in the console.
 
 
 [Documentation: Using loaders](http://webpack.github.io/docs/using-loaders.html)
--- index.js
+++ index.js
@@ -3,23 +3,64 @@
 	Author Tobias Koppers @sokra
 */
 
-function accesorString(value) {
-	var depths = value.split(".");
-	var length = depths.length;
-	var result = "";
-	
-	for (var i = 0; i < length; i++) {
-		result += "[" + JSON.stringify(depths[i]) + "]";
-	}
+var loaderUtils = require("loader-utils");
 
-	return result;
+module.exports = function () {
+};
+
+module.exports.pitch = function (content) {
+  var query,
+      accesorValue;
+
+  this.cacheable();
+
+  if (!this.query) {
+    throw new Error("query parameter is missing");
+  }
+
+  query = loaderUtils.parseQuery(this.query);
+
+  if (query.module) {
+    accesorValue = loaderUtils.interpolateName(this, query.module, {
+      context: query.context || this.options.context,
+      content: content,
+      regExp: query.regExp
+    });
+
+    if (query.upperCamelCase) {
+      accesorValue = accesorValue.split('/').map(toUpperCamelCase).join('.')
+    }
+
+    if (query.ns) {
+      accesorValue = [query.ns, accesorValue].join(['.']);
+    }
+  } else {
+    accesorValue = this.query.substr(1)
+  }
+
+  return accesorString(accesorValue) + " = require(" + JSON.stringify("-!" + content) + ");";
+};
+
+function accesorString(value) {
+  var childProperties = value.split("."),
+      length = childProperties.length,
+      propertyString = "global",
+      result = "";
+
+  for (var i = 0; i < length; i++) {
+    if (i > 0) {
+      result += "if(!" + propertyString + ") " + propertyString + " = {};";
+    }
+    propertyString += "[" + JSON.stringify(childProperties[i]) + "]";
+  }
+
+  result += "module.exports = " + propertyString;
+
+  return result;
 }
 
-module.exports = function() {};
-module.exports.pitch = function(remainingRequest) {
-	this.cacheable && this.cacheable();
-	if(!this.query) throw new Error("query parameter is missing");
-	return "module.exports = " +
-		"global" + accesorString(this.query.substr(1)) + " = " +
-		"require(" + JSON.stringify("-!" + remainingRequest) + ");";
-};
+function toUpperCamelCase(str) {
+  return str.split('-').map(function (a) {
+    return a.substr(0, 1).toUpperCase() + a.substr(1)
+  }).join('');
+}
