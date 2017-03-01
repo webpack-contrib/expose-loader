e08054fe6d3f3dc8571611a1abb06ab3e8f26d0c
James Ribe <jribe@tableau.com>
2017-03-01 13:29:55 -0800

    wrapped everything in a closure that imports buildin/global

--- index.js
+++ index.js
@@ -2,27 +2,33 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-var loaderUtils = require("loader-utils");
-var SourceNode = require("source-map").SourceNode;
-var SourceMapConsumer = require("source-map").SourceMapConsumer;
-var FOOTER = "/*** EXPOSES FROM expose-loader ***/\n";
+const loaderUtils = require("loader-utils");
+const SourceNode = require("source-map").SourceNode;
+const SourceMapConsumer = require("source-map").SourceMapConsumer;
+const FOOTER = "\n/*** EXPOSES FROM expose-loader ***/\n";
 module.exports = function(content, sourceMap) {
 	this.cacheable && this.cacheable();
-	var query = loaderUtils.getOptions(this) || {};
-	var exposes = [];
-	var keys = Object.keys(query);
+	const query = loaderUtils.getOptions(this) || {};
+	const exposes = [];
+	const keys = Object.keys(query);
 	keys.forEach(function(name) {
-		exposes.push("globals[" + JSON.stringify(name) + "] = module.exports;");
+		exposes.push(`globals[${JSON.stringify(name)}] = module.exports;`);
 	});
+	const exposeClosure = `
+	${FOOTER}
+	(function(){
+		const globals = require('webpack/buildin/global');
+		${exposes.join("\n")}
+	})();`
 	if(sourceMap) {
-		var currentRequest = loaderUtils.getCurrentRequest(this);
-		var node = SourceNode.fromStringWithSourceMap(content, new SourceMapConsumer(sourceMap));
-		node.add("\n\n" + FOOTER + exposes.join("\n"));
-		var result = node.toStringWithSourceMap({
+		const currentRequest = loaderUtils.getCurrentRequest(this);
+		const node = SourceNode.fromStringWithSourceMap(content, new SourceMapConsumer(sourceMap));
+		node.add(exposeClosure);
+		const result = node.toStringWithSourceMap({
 			file: currentRequest
 		});
 		this.callback(null, result.code, result.map.toJSON());
 		return;
 	}
-	return content + "\n\n" + FOOTER + exposes.join("\n");
+	return content + exposeClosure;
 }
