2b0a30026e659692d68b7d30219782c106ea4846
James Ribe <jribe@tableau.com>
2017-03-02 15:45:40 -0800

    updated to use 'global' instead of 'globals'

--- index.js
+++ index.js
@@ -5,30 +5,25 @@
 const loaderUtils = require("loader-utils");
 const SourceNode = require("source-map").SourceNode;
 const SourceMapConsumer = require("source-map").SourceMapConsumer;
-const FOOTER = "\n/*** EXPOSES FROM expose-loader ***/\n";
+const FOOTER = "\n/*** EXPOSES FROM expose-loader ***/\n\n";
 module.exports = function(content, sourceMap) {
 	this.cacheable && this.cacheable();
 	const query = loaderUtils.getOptions(this) || {};
 	const exposes = [];
 	const keys = Object.keys(query);
 	keys.forEach(function(name) {
-		exposes.push(`globals[${JSON.stringify(name)}] = module.exports;`);
+		exposes.push(`global[${JSON.stringify(name)}] = module.exports;`);
 	});
-	const exposeClosure = `
-	${FOOTER}
-	(function(){
-		const globals = require('webpack/buildin/global');
-		${exposes.join("\n")}
-	})();`
+	const exposeStatements = `${FOOTER}${exposes.join("\n")}`
 	if(sourceMap) {
 		const currentRequest = loaderUtils.getCurrentRequest(this);
 		const node = SourceNode.fromStringWithSourceMap(content, new SourceMapConsumer(sourceMap));
-		node.add(exposeClosure);
+		node.add(exposeStatements);
 		const result = node.toStringWithSourceMap({
 			file: currentRequest
 		});
 		this.callback(null, result.code, result.map.toJSON());
 		return;
 	}
-	return content + exposeClosure;
+	return content + exposeStatements;
 }
