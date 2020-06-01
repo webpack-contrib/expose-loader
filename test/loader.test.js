1d298ef99a958702ad5b5e0284b38b53f4916b67
evilebottnawi <sheo13666q@gmail.com>
2020-06-01 19:14:30 +0300

    feat: support multiple exposes

--- test/loader.test.js
+++ test/loader.test.js
@@ -40,4 +40,36 @@ describe('loader', () => {
     expect(getErrors(stats)).toMatchSnapshot('errors');
     expect(getWarnings(stats)).toMatchSnapshot('warnings');
   });
+
+  it('should work with multiple exposes', async () => {
+    const compiler = getCompiler('simple-module-default.js', {
+      expose: ['globalObject2', 'globalObject3'],
+    });
+    const stats = await compile(compiler);
+
+    expect(
+      getModuleSource('./global-commonjs2-multiple-exports.js-exposed', stats)
+    ).toMatchSnapshot('module');
+    expect(
+      execute(readAsset('main.bundle.js', compiler, stats))
+    ).toMatchSnapshot('result');
+    expect(getErrors(stats)).toMatchSnapshot('errors');
+    expect(getWarnings(stats)).toMatchSnapshot('warnings');
+  });
+
+  it('should work with multiple exposes with nested properties', async () => {
+    const compiler = getCompiler('simple-module-default.js', {
+      expose: ['globalObject2.foo', 'globalObject3.bar'],
+    });
+    const stats = await compile(compiler);
+
+    expect(
+      getModuleSource('./global-commonjs2-multiple-exports.js-exposed', stats)
+    ).toMatchSnapshot('module');
+    expect(
+      execute(readAsset('main.bundle.js', compiler, stats))
+    ).toMatchSnapshot('result');
+    expect(getErrors(stats)).toMatchSnapshot('errors');
+    expect(getWarnings(stats)).toMatchSnapshot('warnings');
+  });
 });
