52cd900669ada661d79023de3ccf14e5d7aaab97
Sachin Kumar <ersachin3112@gmail.com>
2025-07-29 18:24:42 +0530

    chore: update code

--- src/runtime/getGlobalThis.js
+++ src/runtime/getGlobalThis.js
@@ -4,30 +4,30 @@ module.exports = (function () {
     return globalThis;
   }
 
-  let globalObj;
+  // eslint-disable-next-line id-length
+  let g;
 
   try {
     // This works if eval is allowed (see CSP)
     // eslint-disable-next-line no-new-func
-    globalObj = this || new Function("return this")();
+    g = this || new Function("return this")();
   } catch {
     // This works if the window reference is available
-    // eslint-disable-next-line unicorn/prefer-global-this
+    /* eslint-disable unicorn/prefer-global-this, no-undef */
     if (typeof window === "object") {
-      // eslint-disable-next-line unicorn/prefer-global-this, no-undef
       return window;
     }
-
     // This works if the self reference is available
-    if (typeof globalThis.self === "object") {
-      return globalThis;
+    if (typeof self === "object") {
+      return self;
     }
 
     // This works if the global reference is available
-    if (typeof globalThis.global !== "undefined") {
-      return globalThis;
+    if (typeof global !== "undefined") {
+      return global;
     }
+    /* eslint-enable unicorn/prefer-global-this, no-undef */
   }
 
-  return globalObj;
+  return g;
 })();
